60d0344a80981ca514307a2aaed4d3ad8fea626c
Anonimos0703 <charleswilliam.sevenial@cit.edu>
2025-09-22 21:29:52 +0800

    for deployment

--- CAPSTONE-BACKEND/src/main/java/com/capstone/HRMS/Config/DotenvConfig.java
+++ CAPSTONE-BACKEND/src/main/java/com/capstone/HRMS/Config/DotenvConfig.java
@@ -1,28 +1,28 @@
-package com.capstone.HRMS.Config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertiesPropertySource;
-
-import java.util.Properties;
-
-@Configuration
-public class DotenvConfig {
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(ConfigurableEnvironment environment) {
-        Dotenv dotenv = Dotenv.configure().load();
-
-        Properties props = new Properties();
-
-        dotenv.entries().forEach(entry -> props.put(entry.getKey(), entry.getValue()));
-
-        environment.getPropertySources().addFirst(new PropertiesPropertySource("dotenvProperties", props));
-
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-}
+//package com.capstone.HRMS.Config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+//import org.springframework.core.env.ConfigurableEnvironment;
+//import org.springframework.core.env.PropertiesPropertySource;
+//
+//import java.util.Properties;
+//
+//@Configuration
+//public class DotenvConfig {
+//
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(ConfigurableEnvironment environment) {
+//        Dotenv dotenv = Dotenv.configure().load();
+//
+//        Properties props = new Properties();
+//
+//        dotenv.entries().forEach(entry -> props.put(entry.getKey(), entry.getValue()));
+//
+//        environment.getPropertySources().addFirst(new PropertiesPropertySource("dotenvProperties", props));
+//
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+//}
